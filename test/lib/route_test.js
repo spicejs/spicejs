8a66853703551d8c753b8e1c2eb66c80202f118a
Marcelo Eden <edendroid@gmail.com>
2014-07-23 18:33:32 -0700

    improve router to allow fallback to all matching routes

--- test/lib/route_test.js
+++ test/lib/route_test.js
@@ -1,9 +1,11 @@
 describe("#route", function() {
-  it("set routes", function() {
-    E.route("a", 1);
-    E.route({b: 2, c: 3});
+  it("sets many routes at once", function() {
+    E.route({a: 1, b: 2, c: 3});
 
-    assert.deepEqual(E.route.map, {a: 1, b: 2, c: 3})
+    assert.deepEqual(E.route.map, [
+      {regex: {}, keys: ["match"], callback: 1},
+      {regex: {}, keys: ["match"], callback: 2},
+      {regex: {}, keys: ["match"], callback: 3} ]);
   });
 
   it("matches a route", function() {
@@ -11,36 +13,45 @@ describe("#route", function() {
     assertRoute("/items/{item}", "/items/debby");
     assertRoute("/items/{item}", "#/items/debby");
     assertRoute("/items/{item}", "#!/items/debby");
-    assertRoute(".*/items", "/hi/items");
-    assertRoute(".*/items", "/some/regex/items");
-    assertRoute(".*/items", "/items");
+    assertRoute("*/items", "/hi/items");
+    assertRoute("*/items", "/some/regex/items");
+    assertRoute("*/items", "/items");
   });
 
-  it("works with the old generic routes", function() {
-    var count = 0, routes = [];
-    E.route(function(path){ count++; routes.push(path) });
-    E.route(function(path){ count++ });
-    E.route(function(path){ count++ });
+  it("works with generic routes", function() {
+    var count = 0;
+    E.route(function(){ count++ });
+    E.route(function(){ count++ });
+    E.route(function(){ count++ });
 
     E.route("/any_randow_route") && assert.equal(count, 3);
     E.route("/other_randow_route") && assert.equal(count, 6);
-    assert.deepEqual(routes, ["/any_randow_route", "/other_randow_route"])
+  });
+
+  it("triggers all routes that matches the path", function(){
+    var count = 0, fn = function(){ count++ };
+    E.route.clear();
+    E.route(fn); E.route({"/my*": fn, "/my/way": fn, "*": fn});
+
+    E.route("/my/way");
+    assert.equal(count, 4);
   });
 
   it("matches the params", function() {
     assertRoute("/items?search={q}", "/items?search=hi",
-      {path: "/items?search=hi", q: "hi"});
+      {path: "/items?search=hi", match: "/items?search=hi", q: "hi"});
 
     assertRoute("/items/{item}/{id}", "/items/debby/2",
-      {path: "/items/debby/2", item: "debby", id: "2"});
+      {path: "/items/debby/2", match: "/items/debby/2", item: "debby", id: "2"});
 
-    assertRoute("/my/.*/{item}/{id}", "/my/crazy/cart/debby/2",
-      {path: "/my/crazy/cart/debby/2", item: "debby", id: "2"});
+    assertRoute("/my/*/{item}/{id}", "/my/crazy/cart/2",
+      {path: "/my/crazy/cart/2", match:  "/my/crazy/cart/2", item: "cart", id: "2"});
   });
 
   function assertRoute(route, path, params) {
     var received = false;
-    E.route(route, function(p) {
+
+    E.route.clear()(route, function(p) {
       received = true
       params && assert.deepEqual(p, params)
     });
@@ -48,5 +59,4 @@ describe("#route", function() {
     E.route(path);
     assert(received, "Invalid route to => " + path);
   }
-
 });
