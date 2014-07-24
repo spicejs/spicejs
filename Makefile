9c5348f627d75fd91ee4d663d74174d5c6a8b57e
Marcelo Eden <edendroid@gmail.com>
2014-07-24 10:00:45 -0700

    allow custom builds

--- Makefile
+++ Makefile
@@ -2,16 +2,21 @@ init:
 	bower install
 	echo '(function(E) { "use strict";' > index.js
 	cat lib/* >> index.js
-	cat ext/* >> index.js
 	echo '})(typeof window !== "undefined" ? window.E = {} : exports);' >> index.js
 
-jshint:
-	jshint lib/*.js
+jshint: init
+	jshint index.js
 
 min: init
 	uglifyjs index.js --comments --mangle -o min.js
 
+ext = ""
+custom: init
+	cat index.js > custom.js
+	for e in $(ext) ; do cat "ext/$$e.js" >> custom.js ; done
+	uglifyjs custom.js --comments --mangle -o custom.min.js
+
 test: min
 	node test/node.js
 
-.PHONY: test
+.PHONY: test jshint
--- index.js
+++ index.js
@@ -127,30 +127,4 @@ E.route = (function() {
   route.clear = function() { map = []; return route; };
   return E.observable(route);
 })();
-// Browser Navigation
-if (typeof window !== "undefined") {
-  // redirect to route, push state
-  E.route.on("visit", function(to) {
-    try {
-      history.pushState(null, null, to);
-    } catch (err) {
-      window.location = to[0] === "#" ? to : "#" + to;
-    }
-  }).on("load", function() {
-    this(location.pathname + location.search + location.hash);
-  });
-
-  // Mozilla, Opera and webkit nightlies currently support this event
-  if (document.addEventListener) {
-    document.addEventListener("DOMContentLoaded", function() {
-      E.route.trigger("load");
-    }, false);
-
-  // If IE event model is used
-  } else if (document.attachEvent) {
-    document.attachEvent("onreadystatechange", function() {
-      if (document.readyState === "complete") E.route.trigger("load");
-    });
-  }
-}
 })(typeof window !== "undefined" ? window.E = {} : exports);
