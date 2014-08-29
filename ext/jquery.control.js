7606eda711c21600bfcc33e22c6bb98474002e62
Marcelo Eden <edendroid@gmail.com>
2014-08-29 12:43:26 -0700

    donot bind controllers to empty elements

--- ext/jquery.control.js
+++ ext/jquery.control.js
@@ -1,7 +1,7 @@
 if (typeof jQuery !== "undefined") {
   jQuery.fn.control = function(name, options) {
+    if (!this.length) return this;
     var dataControl = "data-control-" + name.replace(/\W+/g, "-");
-
     if (this.attr(dataControl)) return this;
     this.attr(dataControl, true);
     return S.control(name, this, options);
