305e5d76c80a06adf8cd592cca9e262227df34bd
Marcelo Eden <edendroid@gmail.com>
2014-08-29 13:58:08 -0700

    minor refactoring on $.control

--- ext/jquery.control.js
+++ ext/jquery.control.js
@@ -1,8 +1,7 @@
 if (typeof jQuery !== "undefined") {
   jQuery.fn.control = function(name, options) {
-    if (!this.length) return this;
     var dataControl = "data-control-" + name.replace(/\W+/g, "-");
-    if (this.attr(dataControl)) return this;
+    if (!this.length || this.attr(dataControl)) return this;
     this.attr(dataControl, true);
     return S.control(name, this, options);
   };
